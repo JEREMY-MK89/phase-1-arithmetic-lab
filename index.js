56caa0b4a75adde764eda753b68fc6f9226075d5
moses <moses.kingi@moringaschool.com>
2023-10-06 06:45:33 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
+let num1=1
+let num2=62
+const multiply = num1*num2
 
-// Write your code here
+//solution 2
+
+const random = Math.floor(Math.random()*10)+1
+     
+console.log(!random)
+ 
+  
+  
+
+
+//solution 3
+
+ let num3 = 19
+ let num4 =  5
+const mod = num3 % num4
+
+
+//solution 4
+const max = (4,5,10,20);
+
+console.log (Math.max(max));
+
+
+
+
+
+
+
+
